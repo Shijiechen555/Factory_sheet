c55fd3b963e2cfab0a138700ecbd69fb68b271fe
shijie <shijiechen555@gmail.com>
2025-03-05 13:11:22 -0800

    Fix gunicorn import issue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect,gunicorn
+from flask import Flask, render_template, request, redirect
 import openpyxl
-from datetime import datetime
+import os
+
 
 app = Flask(__name__)
 
